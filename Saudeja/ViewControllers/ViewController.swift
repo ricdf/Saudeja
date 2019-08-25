ffc769629c825940b5c07820f6972deaea4f50d3
Ricardo Cavalcante Barbosa <ricodf@gmail.com>
2019-08-25 08:50:17 -0300

    Instalado o Pod Firebase e SDWebImage

---
+++ Saudeja/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saudeja
+//
+//  Created by Ricardo Cavalcante on 25/08/19.
+//  Copyright © 2019 Ricardo Cavalcante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

2bb7e6d167d2ee6a8a2d1abe7f31e993bdeffc73
Ricardo Cavalcante Barbosa <ricodf@gmail.com>
2019-09-21 01:54:51 -0300

    Continuando o perfilusuario, alguns picker view

---
+++ Saudeja/Models/CustomPickerView/PickerData/DatePickerPopoverViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DatePickerPopoverViewController.swift
+//  SwiftyPickerPopover
+//
+//  Created by Yuta Hoshino on 2016/09/14.
+//  Copyright © 2016 Yuta Hoshino. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DatePickerPopoverViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+    
+    var doneAction: ((Date)->Void)?
+    var cancleAction: (()->Void)?
+    var clearAction: (()->Void)?
+    
+    @IBOutlet weak var picker: UIDatePicker!
+    
+    var selectedDate = Date()
+    var dateMode: UIDatePicker.Mode = .date
+    var hideClearButton: Bool = false
+
+    @IBOutlet weak var clearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.date = selectedDate
+        picker.datePickerMode = dateMode
+     //   clearButton.isHidden = hideClearButton
+    }
+    
+    
+    @IBAction func tappedDone(_ sender: UIButton? = nil) {
+        doneAction?(picker.date)
+        
+        dismiss(animated: true, completion: {})
+    }
+    
+    @IBAction func tappedCancel(_ sender: UIButton? = nil) {
+        cancleAction?()
+        dismiss(animated: true, completion: {})
+    }
+    
+    @IBAction func tappedClear(_ sender: UIButton? = nil) {
+        clearAction?()
+        dismiss(animated: true, completion: {})
+    }
+    
+    /// popover dismissed
+    func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) {
+        tappedCancel()
+    }
+    
+    /// Popover appears on iPhone
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
+    }
+}
